741c75c72e501637978d7f42a0a09b7ff08b0189
eniola <bukkyadewale123@.com>
2025-06-15 23:55:09 +0100

    Add data migration to auto-load questions fixture on deploy

--- game/apps.py
+++ game/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+from django.db import connection
+from django.core.management import call_command
+from django.apps import apps
 
 
 class GameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'game'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "game"
+
+    def ready(self):
+        Question = apps.get_model("game", "Question")
+        try:
+            if connection.introspection.table_names() and Question.objects.count() == 0:
+                call_command("loaddata", "questions.json")
+        except Exception:
+            pass  # Avoid crashing if migrations aren't ready
---
+++ game/migrations/0002_load_questions.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+def load_questions(apps, schema_editor):
+    from django.core.management import call_command
+    try:
+        call_command('loaddata', 'questions.json')
+    except Exception:
+        pass
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('game', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(load_questions),
+    ]
